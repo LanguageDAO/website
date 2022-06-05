65b42b15df59f334c780835a7a84ef53048e9dff
bartosjiri <me@bartosjiri.com>
2022-06-05 20:12:05 +0200

    Add meta

---
+++ src/constants/meta.ts
@@ -0,0 +1,22 @@
+export const META_TITLE = 'LanguageDAO';
+export const META_DESCRIPTION = 'Making web3 accessible for all languages';
+export const META_KEYWORDS = [
+	'LanguageDAO',
+	'DAO',
+	'language',
+	'translation',
+	'translate',
+	'$PEOPLE',
+	'PeopleDAO'
+];
+
+export const META_THEME_COLOR = '#ffffff';
+export const META_APPLE_COLOR = '#2a72f2';
+export const META_MICROSOFT_COLOR = '#ffffff';
+
+export const META_OG_URL = 'https://languagedao.com';
+export const META_OG_IMAGE_URL = `${META_OG_URL}/meta/og-image.jpg`;
+export const META_OG_IMAGE_HEIGHT = 599;
+export const META_OG_IMAGE_WIDTH = 1145;
+
+export const META_VERSION = 'xhknz0vac6';
