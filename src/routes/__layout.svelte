f9d77e0ee8ff9921fe6a1d05e5a6e33c273c34d6
bartosjiri <me@bartosjiri.com>
2022-07-04 16:25:29 +0200

    Add analytics & minor updates

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,13 @@
 <script>
+	import { Analytics } from '$util/analytics';
+
 	import PazlyColors from '$util/scripts/PazlyColors.svelte';
 
 	import '../app.css';
 </script>
 
 <PazlyColors />
+
+<Analytics />
 
 <slot />
